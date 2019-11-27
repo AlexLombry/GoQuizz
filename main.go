7f3c12ea32d21810574b42c3dcfe7d97b8ae9411
Alex Lombry <alex.lombry@gmail.com>
2019-11-28 00:35:23 +0100

    Handle user input and count score

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type Problem struct {
+	question string
+	answer string
+}
+
 func main() {
 	// Generate flag with -h for help
 	filename := flag.String("csv", "file.csv", "CSV file in formation 'question,answer'")
@@ -19,8 +24,40 @@ func main() {
 	}
 
 	// Create the csv reader
-	r := csv.NewReader(file)
-	_ = r
+	reader := csv.NewReader(file)
+
+	// Read all lines
+	lines, err := reader.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("Failed to parse the file"))
+	}
+
+	problems := parseLines(lines)
+	correct := 0
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, problem.question)
+		var answer string
+		// make sur it has a pointer value to it
+		_, _ = fmt.Scanf("%s\n", &answer)
+		if answer == problem.answer {
+			correct++
+		}
+	}
+
+	fmt.Printf("Scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []Problem {
+	r := make([]Problem, len(lines))
+
+	for i, line := range lines {
+		r[i] = Problem{
+			question: line[0],
+			answer: line[1],
+		}
+	}
+
+	return r
 }
 
 func exit(msg string) {
